193895247c4700344a3e1419d2124860fcc2ecd1
Ricky Wilkinson <124752575+UnexpectedSensei@users.noreply.github.com>
2023-03-13 15:51:44 +0000

    Scenes Added
    Scenes and first level added using sprite images

--- Alien Archeologist Unity File/Assets/Scripts/SceneChange.cs
+++ Alien Archeologist Unity File/Assets/Scripts/SceneChange.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
-
-    public string startMessage;
-    public string updateMessage;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(startMessage);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(updateMessage);
+        
     }
 }
